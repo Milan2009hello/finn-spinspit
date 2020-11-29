b37fc5a252ec16a23c0cf49c549c592475298418
Milan2009hello <58265883+Milan2009hello@users.noreply.github.com>
2020-11-29 09:13:59 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("FINN verkade")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Mijn broer heet Milan verkade")
+})
